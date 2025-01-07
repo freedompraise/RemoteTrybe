4adc4097cda09a08fc7299eac6244ddb0eeb2992
Praise Freedom Dike <dikepraise119@gmail.com>
2025-01-07 11:33:16 +0100

    Add responsive navigation menu and slide-in animation

--- script.js
+++ script.js
@@ -27,3 +27,14 @@ window.addEventListener("click", function (event) {
     closeModal();
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const menuButton = document.getElementById("menu-button");
+  const menu = document.getElementById("menu");
+
+  menuButton.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+    menu.classList.toggle("flex");
+    menu.classList.toggle("animate-slide-in");
+  });
+});
