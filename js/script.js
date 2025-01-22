e31a317a4a2831f902d6a5c6834010f67f1ce5fd
Praise Freedom Dike <dikepraise119@gmail.com>
2025-01-22 13:31:38 +0100

    Update payment message to clarify intent for joining VA training cohort

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 document.getElementById("pay-now").addEventListener("click", function () {
   var fullName = document.getElementById("name").value;
-  var message = `Hello, I am ${fullName}. I want to make a payment for the training.`;
+  var message = `Hello, I am ${fullName}. I want to join 
+  the next cohort of the VA training.
+  `;
   if (fullName) {
     var whatsappUrl =
       "https://wa.me/2349060038374?text=" + encodeURIComponent(message);
